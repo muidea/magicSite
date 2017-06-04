61d286c2eba078bef786e0ae76aa0f7bc029c258
muidea <muidea@gmail.com>
2017-06-04 22:39:07 +0800

    add group.js

---
+++ src/mock/group.js
@@ -0,0 +1,161 @@
+const qs = require('qs')
+const Mock = require('mockjs')
+const config = require('../utils/config')
+const { apiPrefix } = config
+
+let groupsListData = Mock.mock({
+  'data|80-100': [
+    {
+      id: '@id',
+      name: '@name',
+      description: '@description',
+      'catalog|0-2':1,
+      avatar () {
+        return Mock.Random.image('100x100', Mock.Random.color(), '#757575', 'png', this.name.substr(0, 1))
+      },
+    },
+  ],
+})
+
+
+let database = groupsListData.data
+
+const queryArray = (array, key, keyAlias = 'key') => {
+  if (!(array instanceof Array)) {
+    return null
+  }
+  let data
+
+  for (let item of array) {
+    if (item[keyAlias] === key) {
+      data = item
+      break
+    }
+  }
+
+  if (data) {
+    return data
+  }
+  return null
+}
+
+const NOTFOUND = {
+  message: 'Not Found',
+  documentation_url: 'http://localhost:8000/request',
+}
+
+module.exports = {
+
+  [`GET ${apiPrefix}/group`] (req, res) {
+    const cookie = req.headers.cookie || ''
+    const cookies = qs.parse(cookie.replace(/\s/g, ''), { delimiter: ';' })
+    const response = {}
+    const group = {}
+    if (!cookies.token) {
+      res.status(200).send({ message: 'Not Login' })
+      return
+    }
+    const token = JSON.parse(cookies.token)
+    if (token) {
+      response.success = token.deadline > new Date().getTime()
+    }
+    if (response.success) {
+      const groupItem = adminUsers.filter(_ => _.id === token.id)
+      if (groupItem.length > 0) {
+        group.permissions = groupItem[0].permissions
+        group.groupname = groupItem[0].groupname
+        group.id = groupItem[0].id
+      }
+    }
+    response.group = group
+    res.json(response)
+  },
+
+  [`GET ${apiPrefix}/groups`] (req, res) {
+    const { query } = req
+    let { pageSize, page, ...other } = query
+    pageSize = pageSize || 10
+    page = page || 1
+
+    let newData = database
+    for (let key in other) {
+      if ({}.hasOwnProperty.call(other, key)) {
+        newData = newData.filter((item) => {
+          if ({}.hasOwnProperty.call(item, key)) {
+            if (key === 'address') {
+              return other[key].every(iitem => item[key].indexOf(iitem) > -1)
+            } else if (key === 'createTime') {
+              const start = new Date(other[key][0]).getTime()
+              const end = new Date(other[key][1]).getTime()
+              const now = new Date(item[key]).getTime()
+
+              if (start && end) {
+                return now >= start && now <= end
+              }
+              return true
+            }
+            return String(item[key]).trim().indexOf(decodeURI(other[key]).trim()) > -1
+          }
+          return true
+        })
+      }
+    }
+
+    res.status(200).json({
+      data: newData.slice((page - 1) * pageSize, page * pageSize),
+      total: newData.length,
+    })
+  },
+
+  [`POST ${apiPrefix}/group`] (req, res) {
+    const newData = req.body
+    newData.createTime = Mock.mock('@now')
+    newData.avatar = newData.avatar || Mock.Random.image('100x100', Mock.Random.color(), '#757575', 'png', newData.nickName.substr(0, 1))
+    newData.id = Mock.mock('@id')
+
+    database.unshift(newData)
+
+    res.status(200).end()
+  },
+
+  [`GET ${apiPrefix}/group/:id`] (req, res) {
+    const { id } = req.params
+    const data = queryArray(database, id, 'id')
+    if (data) {
+      res.status(200).json(data)
+    } else {
+      res.status(404).json(NOTFOUND)
+    }
+  },
+
+  [`DELETE ${apiPrefix}/group/:id`] (req, res) {
+    const { id } = req.params
+    const data = queryArray(database, id, 'id')
+    if (data) {
+      database = database.filter((item) => item.id !== id)
+      res.status(204).end()
+    } else {
+      res.status(404).json(NOTFOUND)
+    }
+  },
+
+  [`PATCH ${apiPrefix}/group/:id`] (req, res) {
+    const { id } = req.params
+    const editItem = req.body
+    let isExist = false
+
+    database = database.map((item) => {
+      if (item.id === id) {
+        isExist = true
+        return Object.assign({}, item, editItem)
+      }
+      return item
+    })
+
+    if (isExist) {
+      res.status(201).end()
+    } else {
+      res.status(404).json(NOTFOUND)
+    }
+  },
+}
