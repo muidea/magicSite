d1583e28747a466e1bffb5d0384d8eed6504d9f6
muidea <rangh@foxmail.com>
2018-03-26 23:42:24 +0800

    upgrade antd from 2.11.2 to 3.3.3

--- src/routes/authority/user/detail/index.js
+++ src/routes/authority/user/detail/index.js
@@ -1,17 +1,33 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'dva'
-import { Card } from 'antd'
+import { Table } from 'antd'
 import styles from './index.less'
 
 const Detail = ({ userDetail }) => {
   const { name, moduleAuthGroup } = userDetail
+
+  const columns = [{
+    title: '模块',
+    dataIndex: 'name',
+    key: 'name',
+  }, {
+    title: '授权组',
+    dataIndex: 'authGroup',
+    key: 'authGroup',
+    render: (text, record) => {
+      const { authGroup } = record
+      const { description } = authGroup
+      return <p>{ description }</p>
+    },
+  }]
 
   return (<div className="content-inner">
     <div className={styles.content}>
       <div className={styles.item}>
         {name}
       </div>
+      <Table pagination={false} columns={columns} dataSource={moduleAuthGroup} />
     </div>
   </div>)
 }
--- src/utils/request.js
+++ src/utils/request.js
@@ -1,7 +1,6 @@
 /* global window */
 import axios from 'axios'
 import qs from 'qs'
-import jsonp from 'jsonp'
 import lodash from 'lodash'
 import pathToRegexp from 'path-to-regexp'
 import { message } from 'antd'
