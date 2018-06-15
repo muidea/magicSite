59e1b40f20a50a790a1de5c00f9fb454b4370791
muidea <rangh@foxmail.com>
2018-06-15 13:53:15 +0800

    update code

--- webpack.config.js
+++ webpack.config.js
@@ -43,7 +43,6 @@ module.exports = (webpackConfig, env) => {
     components: `${__dirname}/src/components`,
     utils: `${__dirname}/src/utils`,
     config: `${__dirname}/src/utils/config`,
-    enums: `${__dirname}/src/utils/enums`,
     services: `${__dirname}/src/services`,
     models: `${__dirname}/src/models`,
     routes: `${__dirname}/src/routes`,
