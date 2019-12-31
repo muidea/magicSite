b4a4c9c3b070d9011aab9c836742707f528d1aaf
muidea <rangh@foxmail.com>
2019-12-31 18:12:49 +0800

    update code

--- src/routes/content/catalog/Modal.js
+++ src/routes/content/catalog/Modal.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Form, Input, Modal } from 'antd'
+import { Form, Input, TreeSelect, Modal } from 'antd'
 
 const FormItem = Form.Item
 const { TextArea } = Input
@@ -36,19 +36,50 @@ const modal = ({
     onOk: handleOk,
   }
 
+  const treeData = [
+    {
+      title: 'Node1',
+      value: '0-0',
+      key: '0-0',
+      children: [
+        {
+          title: 'Child Node1',
+          value: '0-0-1',
+          key: '0-0-1',
+        },
+        {
+          title: 'Child Node2',
+          value: '0-0-2',
+          key: '0-0-2',
+        },
+      ],
+    },
+    {
+      title: 'Node2',
+      value: '0-1',
+      key: '0-1',
+    },
+  ]
+
   return (
     <Modal {...modalOpts}>
       <Form layout="horizontal">
         <FormItem label="分类名" hasFeedback {...formItemLayout}>
-          {getFieldDecorator('name', {
-            initialValue: item.name,
-            rules: [
-              { required: true },
-            ],
-          })(<Input />)}
+          {getFieldDecorator('name', { initialValue: item.name, rules: [{ required: true }] })(<Input />)}
         </FormItem>
         <FormItem label="描述" {...formItemLayout}>
           {getFieldDecorator('description', { initialValue: item.description })(<TextArea rows={3} cols={3} />)}
+        </FormItem>
+        <FormItem label="父分类" {...formItemLayout}>
+          {getFieldDecorator('catalog', { initialValue: item.catalog })(
+            <TreeSelect
+              style={{ width: '100%' }}
+              dropdownStyle={{ maxHeight: 400, overflow: 'auto' }}
+              treeData={treeData}
+              placeholder="请选择父分类"
+              treeDefaultExpandAll
+            />,
+          )}
         </FormItem>
       </Form>
     </Modal>
